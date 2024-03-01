9974cdf73b17a0da225e27a150a2b9cf916f9d65
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-03-01 17:19:04 +0800

    refactor: admin linted format

--- app/admin/home/layout.tsx
+++ app/admin/home/layout.tsx
@@ -8,22 +8,22 @@ import { useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  const { user } = useAuth();
-  const router = useRouter();
-  useEffect(() => {
-    if (user === null) {
-      router.push("/admin/login");
-    }
-  }, [user]);
-  return (
-    <>
-      <div className="h-screen flex flex-col">
-        <AdminNavbar />
-        <EventDataContextProvider>{children}</EventDataContextProvider>
-        <Footer />
-      </div>
-    </>
-  );
+	const { user } = useAuth();
+	const router = useRouter();
+	useEffect(() => {
+		if (user === null) {
+			router.push("/admin/login");
+		}
+	}, [user]);
+	return (
+		<>
+			<div className="flex flex-col">
+				<AdminNavbar />
+				<EventDataContextProvider>{children}</EventDataContextProvider>
+				<Footer />
+			</div>
+		</>
+	);
 };
 
 export default Layout;
--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -2,13 +2,13 @@ import { DataTable } from "@/components/DataTable";
 import { columns } from "@/components/ui/columns";
 
 const Page = () => {
-  return (
-    <>
-      <div className="h-full px-28 py-2">
-        <DataTable columns={columns} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="h-full px-6 py-2">
+				<DataTable columns={columns} />
+			</div>
+		</>
+	);
 };
 
 export default Page;
